c2e3edfeb9b90bd2b6312f4f5c4d25dbecd79a0e
TingtingChen <314896442@qq.com>
2019-11-26 13:07:37 +0800

    day19 test

---
+++ src/main/java/com/java/test/util/day19/BufferedReaderTest05.java
@@ -0,0 +1,22 @@
+package com.java.test.util.day19;
+
+import java.io.Reader;
+
+/**
+ * Created by chentingting07 on 2019/11/26.
+ */
+public class BufferedReaderTest05 {
+}
+
+class MyBufferedReader{
+    private Reader reader;
+    MyBufferedReader(Reader r){
+        this.reader = r;
+    }
+    public String myReadLine(){
+        int buf = 0;
+        while (true){
+
+        }
+    }
+}
