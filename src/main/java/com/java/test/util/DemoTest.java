0d78b668b6efd0219a56fbaccb98acaf5a4998bb
TingtingChen <314896442@qq.com>
2019-11-29 09:44:56 +0800

    day19 lession 12

--- src/main/java/com/java/test/util/DemoTest.java
+++ src/main/java/com/java/test/util/DemoTest.java
@@ -1,10 +1,7 @@
 package com.java.test.util;
 
 import java.util.Iterator;
-<<<<<<< HEAD
-=======
 import java.util.Map;
->>>>>>> 2e9856897618f36e2af433128d2a398cbad4e6ec
 import java.util.Set;
 import java.util.TreeMap;
 
@@ -18,7 +15,6 @@ public class DemoTest {
 
     }
     public static String charCount(String s){
-<<<<<<< HEAD
         char[] arr = s.toCharArray();
         TreeMap<Character, Integer> treeMap = new TreeMap<Character, Integer>();
         for(int x= 0; x<arr.length; x++){
@@ -28,35 +24,12 @@ public class DemoTest {
              else
                  treeMap.put(arr[x],1);
         }
-        StringBuilder sb = new StringBuilder();
-        Set<Character> keyset = treeMap.keySet();
-        Iterator<Character> it = keyset.iterator();
-        while (it.hasNext()){
-            Character key = it.next();
-            Integer value = treeMap.get(key);
-            sb.append(key + ":" + value);
-            if (it.hasNext())
-                sb.append(",");
-=======
-        char[] c = s.toCharArray();
-        TreeMap<Character, Integer> treeMap = new TreeMap<Character, Integer>();
-        int count = 0;
-        for (int x=0; x<c.length; x++){
-            Integer v = treeMap.get(c[x]);
-            if(v!=null){
-                count = v;
-            }
-            count++;
-            treeMap.put(c[x],count);
-            count = 0;
-        }
         Set<Map.Entry<Character, Integer>> entrySet = treeMap.entrySet();
         Iterator<Map.Entry<Character, Integer>> it = entrySet.iterator();
         StringBuffer sb = new StringBuffer();
         while (it.hasNext()){
             Map.Entry<Character, Integer> m = it.next();
             sb.append(m.getKey()+":" + m.getValue()+",");
->>>>>>> 2e9856897618f36e2af433128d2a398cbad4e6ec
         }
         return sb.toString();
     }
--- src/main/java/com/java/test/util/day19/CopyPictureDemo12.java
+++ src/main/java/com/java/test/util/day19/CopyPictureDemo12.java
@@ -1,7 +1,41 @@
 package com.java.test.util.day19;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Created by chentingting07 on 2019/11/29.
  */
 public class CopyPictureDemo12 {
+    public static void main(String[] args){
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try{
+            fis = new FileInputStream("mypicture.jpeg");
+            fos = new FileOutputStream("copypicture.jpeg");
+            byte[] buf = new byte[1024];
+            int len = 0;
+            while ((len = fis.read(buf))!= -1){
+                fos.write(buf);
+            }
+        }catch (IOException e){
+            throw new RuntimeException("图片拷贝失败");
+        }finally {
+            if(fis != null){
+                try{
+                    fis.close();
+                }catch (IOException e){
+                    throw new RuntimeException("图片拷贝失败");
+                }
+            }
+            if(fos != null){
+                try{
+                    fos.close();
+                } catch (IOException e){
+                    throw new RuntimeException("图片拷贝失败");
+                }
+            }
+        }
+    }
 }
